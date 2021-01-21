6f56f2597f5ee9779deb52380e7d7a4d8b825f56
paul-lindberg <paul.lindberg@mymail.champlain.edu>
2021-01-20 19:35:37 -0600

    add improvements to chat history, add No Calculations message

--- methods/calculate.php
+++ methods/calculate.php
@@ -4,14 +4,14 @@ try {
     $db = new PDO(DB_DATASOURCE, DB_USERNAME, DB_PASSWORD);
     header("Content-Type: application/json; charset=UTF-8");
     $obj = json_decode($_POST["params"], false);
-    $stmt = $db->prepare("SELECT * FROM calculations LIMIT ? ?");
-    $stmt->bind_param(1, $obj->limit);
-    $stmt->bind_param(2, $obj->sort);
+    $stmt = $db->prepare("SELECT * FROM calculations ORDER BY calculations.timestamp DESC LIMIT ?");
+//    $stmt->bindValue(1, $obj->sort, PDO::PARAM_STR);
+    $stmt->bindValue(1, $obj->limit, PDO::PARAM_INT);
     $stmt->execute();
-    $result = $stmt->fetchAll(MYSQLI_ASSOC);
+    $result = $stmt->fetchAll();
     echo json_encode($result);
 } catch (PDOException $e) {
-    print_r("Sorry, Nothing");// test
+    print "Sorry, Nothing: " . $e->getMessage();// test
 }
 //maximum result: 2147483647
 //minimum result: -2147483648
