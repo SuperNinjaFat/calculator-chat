1a6b6bc8557121815a6da7472dc1d103e06ad4c2
paul-lindberg <paul.lindberg@mymail.champlain.edu>
2021-01-20 18:40:07 -0600

    add db config, add error status, add table filling

--- methods/calculate.php
+++ methods/calculate.php
@@ -1,1 +1,17 @@
 <?php
+require_once "config.php";
+try {
+    $db = new PDO(DB_DATASOURCE, DB_USERNAME, DB_PASSWORD);
+    header("Content-Type: application/json; charset=UTF-8");
+    $obj = json_decode($_POST["params"], false);
+    $stmt = $db->prepare("SELECT * FROM calculations LIMIT ? ?");
+    $stmt->bind_param(1, $obj->limit);
+    $stmt->bind_param(2, $obj->sort);
+    $stmt->execute();
+    $result = $stmt->fetchAll(MYSQLI_ASSOC);
+    echo json_encode($result);
+} catch (PDOException $e) {
+    print_r("Sorry, Nothing");// test
+}
+//maximum result: 2147483647
+//minimum result: -2147483648
---
+++ methods/config.php
@@ -0,0 +1,7 @@
+<?php
+/* Database credentials. Assuming you are running MySQL server. */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'calculator');
+define('DB_DATASOURCE', 'mysql:host=' . DB_SERVER . ';dbname=' . DB_NAME);
