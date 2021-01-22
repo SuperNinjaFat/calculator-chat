60a7ee78f304eb7e9e338971c8e4bc3fc55572ef
paul-lindberg <paul.lindberg@mymail.champlain.edu>
2021-01-22 16:34:44 -0700

    assign one more paulmlindberg website specific db param

--- methods/config.php
+++ methods/config.php
@@ -3,5 +3,5 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'paulmlin_album');
 define('DB_PASSWORD', 'paulmlin_album');
-define('DB_NAME', 'calculator');
+define('DB_NAME', 'paulmlin_album');
 define('DB_DATASOURCE', 'mysql:host=' . DB_SERVER . ';dbname=' . DB_NAME);
