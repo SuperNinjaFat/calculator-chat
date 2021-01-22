ca7e81f4ce5a6c1e1f7bf2216b5e06061c581b97
paul-lindberg <paul.lindberg@mymail.champlain.edu>
2021-01-22 16:31:50 -0700

    assign paulmlindberg website specific db params

--- methods/config.php
+++ methods/config.php
@@ -1,7 +1,7 @@
 <?php
 /* Database credentials. Assuming you are running MySQL server. */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'paulmlin_album');
+define('DB_PASSWORD', 'paulmlin_album');
 define('DB_NAME', 'calculator');
 define('DB_DATASOURCE', 'mysql:host=' . DB_SERVER . ';dbname=' . DB_NAME);
